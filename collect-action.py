1c72326c47642373aba3d8a261a356a47876a86e
hanasplt <jannaesmeralda8@gmail.com>
2026-02-03 18:43:25 +0800

    working assessment trial for easy level + added more dynamic signs

--- collect-action.py
+++ collect-action.py
@@ -6,8 +6,8 @@ import time
 DATA_DIR = './data_videos'
 os.makedirs(DATA_DIR, exist_ok=True)
 
-GESTURE_CLASSES = ['Ñ']  # Add more gesture names as needed
-VIDEOS_PER_CLASS = 5
+GESTURE_CLASSES = ['WHEN']  # Add more gesture names as needed
+VIDEOS_PER_CLASS = 10
 VIDEO_DURATION = 2  # seconds per video
 FPS = 30
 
